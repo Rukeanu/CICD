2f554de8b9e4a4e0d90c6ee03b56e1acc813c297
Everette Lavona <everettl@mi-w3.msk.21-school.ru>
2024-04-30 14:09:36 +0300

    45 push part 5

--- src/testfail.sh
+++ src/testfail.sh
@@ -10,7 +10,7 @@ rm -rf res_cat.txt
 cd ../grep
 bash run_tests.sh &> res_grep.txt
 RES_G=$(grep -c "FAILED" res_grep.txt)
-if [ "$RES_C" -ne 0 ]; then
+if [ "$RES_G" -ne 0 ]; then
     exit 1
 fi
 rm -rf res_grep.txt
