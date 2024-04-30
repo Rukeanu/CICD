5130d38d12ba3a40353de8efe859bfa9a60733a4
Everette Lavona <everettl@mi-w3.msk.21-school.ru>
2024-04-30 17:17:10 +0300

    47 push part 5

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-scp cat/s21_cat cd@192.168.100.15:/usr/local/bin/
-scp grep/s21_grep cd@192.168.100.15:/usr/local/bin/
+scp cat/s21_cat cd@192.168.100.15:/usr/local/bin/ || exit 1
+scp grep/s21_grep cd@192.168.100.15:/usr/local/bin/ || exit 1
 
 ssh cd@192.168.100.15 ls -lah /usr/local/bin/
+if [ $? -ne 0 ]; then
+    echo "Ошибка при подключении по SSH"
+    exit 1
+fi
 
-#scp (secure copy) для копирования файлов из директории, указанной в LOCAL_DIR_1, \
+#scp (secure copy) для копирования файлов из локальной директории,
 #на удаленную виртуальную машину в директорию, указанную в REMOTE_DIR.
